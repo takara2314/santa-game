61d1bc8f32389f8b16c133e7a60fddb1708cb76f
takara2314 <takara2314_TK@outlook.jp>
2021-12-13 08:06:57 +0900

    テンプレートから作成

---
+++ santa-game/Common.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+// シーンの名前
+enum class State
+{
+	Title,
+	Game
+};
+
+// ゲームデータ
+struct GameData
+{
+	// ハイスコア
+	int32 highScore = 0;
+};
+
+// シーン管理クラス
+using SantaGame = SceneManager<State, GameData>;
